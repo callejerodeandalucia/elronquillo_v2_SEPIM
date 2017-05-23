99db4cac1cbeb600ca86dcd97c22a4cf8f36a839
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-05-23 08:55:17 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,11 +75,14 @@ function geolocalizar(){
 			  cargarCategoria();
 		  };
 		  var errorFunction = function(){
-			  alert("Se ha producido un error al geolocalizar");
+			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio:El Ronquillo");
 			  loading(false);
 		  };
 		  loading(true);
-		  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+		  navigator.geolocation.getCurrentPosition(successFunction, errorFunction,{
+			maximumAge: 75000,
+			timeout: 7000
+		  });
 		} else {
 		  alert("El navegador utilizado no soporta la geolocalización");
 		}
